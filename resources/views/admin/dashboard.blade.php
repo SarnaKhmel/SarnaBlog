51fdf1001a4f0ebd9446d4c542babaf929c71cbc
SarnaKhmel <olsarnat@gmail.com>
2017-11-22 00:31:32 +0200

    4th category and materials

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,52 @@
-    @extends('layouts.app');
+    @extends('admin.layouts.app_admin');
 
 
         @section('content');
-        <h1> Hi motherfucker! </h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="jombotron">
+                        <p> <span class="label label-primary">Категорій 0</span></p>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="jombotron">
+                        <p> <span class="label label-primary">Матеріалів 0</span></p>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="jombotron">
+                        <p> <span class="label label-primary">Користувачів 0</span></p>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="jombotron">
+                        <p> <span class="label label-primary">Сьогодні 0</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#"> Створити категорію</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading"> Перша категорія</h4>
+                    <p class="list-group-item-text">
+                        Кіл-ть категорій
+                    </p>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#"> Створити матеріал</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading"> Перший матеріал</h4>
+                    <p class="list-group-item-text">
+                        Кіл-ть матеріалів
+                    </p>
+                </a>
+            </div>
+        </div>
         @endsection
